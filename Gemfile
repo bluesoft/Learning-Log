6438fdf7834bb9b6eae7793559a12c17e0207292
Ismael <ismael@bluesoft.com.br>
2011-07-08 07:55:21 -0300

    creating the project

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3'
+gem 'unicorn'
+gem 'capistrano'
+gem 'haml'
+gem 'nokogiri'
+gem "compass", ">= 0.11.3"
+
+group :development, :test do
+  gem 'rspec-rails', '2.6.1'
+  gem 'rspec-expectations'
+end
+
+group :test do  
+  gem 'guard'
+  gem 'guard-spork'  
+  gem 'spork', '~> 0.9.0.rc'  
+  gem 'rb-fsevent'
+  gem 'growl'
+  gem 'rcov'  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningLog::Application.config.secret_token = '9798559182d5d19c1b9579ceaa6ff738b243d55fc038451aabb73edee6b79dc20659bfffc161ddb662ec225098adcf836956df838c5f588f9edfc050e3f1e55c'
